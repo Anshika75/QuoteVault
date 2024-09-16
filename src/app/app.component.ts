338dc74f28a75bfb0aeb1eac847d46fa8f889e85
Demi Lune <anshikathakur596@gmail.com>
2024-09-16 15:32:52 +0530

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { TagsComponent } from "./tags/tags.component";
 import { TagData } from "./tags/tags";
+import { QuoteBoxComponent } from "./quote-box/quote-box.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, TagsComponent],
+  imports: [HeaderComponent, TagsComponent, QuoteBoxComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
