2bae200416f643eb3540d2c46acdfd368236fb46
Demi Lune <anshikathakur596@gmail.com>
2024-09-16 14:59:27 +0530

    tags

--- src/app/tags/tags.ts
+++ src/app/tags/tags.ts
@@ -1,1 +1,10 @@
-export const tags = 
+export const tags = [
+    { "id": 1, "name": "Inspiration" },
+    { "id": 2, "name": "Humor" },
+    { "id": 3, "name": "Perseverance" },
+    { "id": 4, "name": "Life" },
+    { "id": 5, "name": "Success" },
+    { "id": 6, "name": "Love" },
+    { "id": 7, "name": "Wisdom" }
+  ]
+  
