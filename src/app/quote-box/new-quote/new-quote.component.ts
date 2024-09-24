70287d9a6f8332707573b00f193c7501d0141343
Demi Lune <anshikathakur596@gmail.com>
2024-09-24 15:04:48 +0530

    ng model done

--- src/app/quote-box/new-quote/new-quote.component.ts
+++ src/app/quote-box/new-quote/new-quote.component.ts
@@ -1,15 +1,17 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-quote',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-quote.component.html',
   styleUrl: './new-quote.component.css'
 })
 export class NewQuoteComponent {
   @Output() close = new EventEmitter<void>();
-
+  enteredAuthor = '';
+  enteredQuote = '';
   onClose() {
     this.close.emit();
   }
