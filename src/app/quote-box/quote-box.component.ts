f21b588c727e15177c7ee54a1b48754b825e95d0
Demi Lune <anshikathakur596@gmail.com>
2024-09-24 15:30:38 +0530

    doneeeeeeeee

--- src/app/quote-box/quote-box.component.ts
+++ src/app/quote-box/quote-box.component.ts
@@ -3,6 +3,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteData } from './quote/quote';
 import { TagsComponent } from '../tags/tags.component';
 import { NewQuoteComponent } from "./new-quote/new-quote.component";
+import { newQuoteType } from './quote/quote.model';
 
 @Component({
   selector: 'app-quote-box',
@@ -18,8 +19,8 @@ export class QuoteBoxComponent {
   // selectedTagId?: number;
   quote = QuoteData;
   isAddingQuote = false;
+  value: any;
 
-  // Fix the filtering logic to filter by tagId, not quote id
   get selectedTagQuote() {
     // return this.quote.filter((quote) => quote.tagId === this.tagSelected);
     return this.quote.filter((quote) => quote.tagId === this.tagId);
@@ -33,7 +34,17 @@ export class QuoteBoxComponent {
   onCanceledAddingQuote() {
     this.isAddingQuote = false;
   }
-  
+
+  onAddQuote(quoteData: newQuoteType) {
+    this.quote.unshift({
+      id: new Date().getTime(),
+      tagId: this.tagId ?? 1,
+      quote: quoteData.quote,
+      author: quoteData.author,
+    });
+    this.isAddingQuote = false;
+  }
+
   onDeleteQuote(quoteId: number) {
     this.quote = this.quote.filter((quote) => quote.id !== quoteId);
   }
