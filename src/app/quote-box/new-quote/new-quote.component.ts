c3915756c98b03f6cc6e2507a2f1725637dd0cd0
Demi Lune <anshikathakur596@gmail.com>
2024-09-24 14:18:25 +0530

    new quote component added

---
+++ src/app/quote-box/new-quote/new-quote.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-quote',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-quote.component.html',
+  styleUrl: './new-quote.component.css'
+})
+export class NewQuoteComponent {
+
+}
